af6d366c97b6090c78a9ee86bbde7321cc4af271
guotie <guotie.9@gmail.com>
2021-05-12 16:31:38 +0800

    mint test function

--- test/shared/mint.ts
+++ test/shared/mint.ts
@@ -1,5 +1,9 @@
+// ctoken mint
+const hre = require('hardhat')
+const ethers = hre.ethers
 
+import { ContractAddrAbi } from '../../deployments/deploys'
 
-export default async () => {
+export default async (cont: ContractAddrAbi, ctoken: string) => {
 
 }
