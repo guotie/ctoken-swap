238690725c83b4606e6421f9f4c51cd15ed3507f
guotie <guotie.9@gmail.com>
2021-05-12 16:33:15 +0800

    mint

--- test/shared/mint.ts
+++ test/shared/mint.ts
@@ -5,5 +5,6 @@ const ethers = hre.ethers
 import { ContractAddrAbi } from '../../deployments/deploys'
 
 export default async (cont: ContractAddrAbi, ctoken: string) => {
-
+  const namedSigners = await ethers.getSigners()
+  let c = new ethers.Contract(cont.address, cont.abi, namedSigners[0])
 }
