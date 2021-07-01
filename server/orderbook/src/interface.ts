2abef5c7ca21a4ae0e96bfeccd65766428576cf8
guotie <guotie.9@gmail.com>
2021-07-01 20:54:22 +0800

    文档

--- server/orderbook/src/interface.ts
+++ server/orderbook/src/interface.ts
@@ -1,3 +1,5 @@
+import { BigNumber, BigNumberish } from "ethers";
+
 /**
  * @description User-Service parameters
  */
@@ -15,4 +17,24 @@ export interface ISwapRequest {
   fromTokenAddress: string
   toTokenAddress: string
   amount: string
+}
+
+
+// 返回给定交易对及价格的请求
+export interface LimitOrderReq {
+  src: string    // 卖出币种
+  dest: string   // 买入币种
+  amtIn: BigNumberish  // src 的数量
+  amtOut: BigNumberish // 预期得到的dest数量
+}
+
+// 待成交的订单
+export interface OrderToTake {
+  amt: BigNumber
+  orderId: string
+}
+
+// 返回特定价格的订单及成交数量列表
+export interface LimitOrderResp {
+  orders: OrderToTake[]
 }
