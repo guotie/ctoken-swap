8edcc8720dfd47ea4c973bb07cd0d2d909d9951f
guotie <guotie.9@gmail.com>
2021-10-24 11:29:46 +0800

    remove console.log

--- contracts/swap/amm/Pair.sol
+++ contracts/swap/amm/Pair.sol
@@ -23,7 +23,7 @@ import "../interface/ICToken.sol";
 
 import "./PairStorage.sol";
 
-import "hardhat/console.sol";
+// import "hardhat/console.sol";
 
 // interface ICToken {
 
@@ -494,7 +494,7 @@ contract DeBankPair is IDeBankPair, PairStorage {
         // LP 抵押需要使用专门的方法
         // address lpDepositAddr;
         // require(msg.sender != lpDepositAddr, "burn");
-        console.log("burn start");
+        // console.log("burn start");
         (uint112 _reserve0, uint112 _reserve1,) = getReserves();
         // gas savings
         address _token0 = cToken0;
